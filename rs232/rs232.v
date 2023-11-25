2ec4940f1ade125899dbaffaef7160a870f580ca
Angel Lozano <angel_ivan_5@outlook.com>
2023-11-25 14:33:20 -0600

    Testing baudrate gen in hardware

--- rs232/rs232.v
+++ rs232/rs232.v
@@ -1,3 +1,28 @@
-module rs232 (input x, output y);
-	assign y = x;
+module rs232 (input             reset,
+				  input             clk,
+				  input      [1:0]  sel,
+				  output            clk_out,
+				  output            reset_led
+				  );
+	
+	reg [15:0] value;
+	
+	always @(sel) begin
+		case (sel)
+			2'b00:	value <= 433;
+			2'b01:	value <= 867;
+			2'b10:	value <= 1301;
+			2'b11:	value <= 5207;
+		endcase
+	end
+	
+	baudrate_generator bg(
+		.reset(reset),
+		.clk(clk),
+		.baud_value(value),
+		.clk_out(clk_out)
+	);
+	
+	assign reset_led = reset;
+	
 endmodule 
